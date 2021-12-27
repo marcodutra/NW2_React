26dfa2884e5dffac09d9f7d6aa357778b49c106c
marco <marco.dutra99@hotmail.com>
2021-12-27 15:44:34 -0300

    Finalizando banco de dados

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,56 @@
 import express from 'express';
+import db from './database/connection';
+import convertHourToMinutes from './utils/convertHourToMinutes';
 
 const routes  = express.Router();
 
-routes.post('/classes', (request, response) => {
-    const data = request.body;
+interface ScheduleItem {
+    week_day: number;
+    from: string;
+    to: string;
+}
 
-    console.log(data);
+routes.post('/classes', async (request, response) => {
+    const {
+        name, 
+        avatar,
+        whatsapp,
+        bio,
+        subject,
+        cost, 
+        schedule
+    } = request.body;
+
+   const insertedUsersIds = await db('users').insert({
+        name,
+        avatar,
+        whatsapp,
+        bio,
+    })
+
+    const user_id = insertedUsersIds[0];
+
+     const insertedClassesIds = await db('classes').insert({
+        subject,
+        cost,
+        user_id
+    })
+
+    const class_id =  insertedClassesIds[0];
+
+    const  classSchedule = schedule.map((scheduleItem: ScheduleItem) =>{
+        return {
+            class_id,
+            week_day: scheduleItem.week_day,
+            from: convertHourToMinutes(scheduleItem.from),
+            to: convertHourToMinutes(scheduleItem.to)
+        };
+    })
+
+     await db('class_schedule').insert(classSchedule);
+
 
     return response.send();
-
 });
 
 export default routes;
