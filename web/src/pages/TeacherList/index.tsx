ebc4b2b00b3b533a7094a659d6428fece358cf82
marco <marco.dutra99@hotmail.com>
2021-12-29 11:53:20 -0300

    Ajuste na pagina Dar aulas

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
+import Input from "../../components/input";
 
 import './styles.css'
 
@@ -9,22 +10,11 @@ function TeacherList(){
     return(
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os Proffys disponíveis.">
-                <form id="search-teachers">
-                   <div className="input-block">
-                     <label htmlFor="subject">Mateŕia</label>
-                     <input type="text" id="subject" />
-                   </div>  
-
-                   <div className="input-block">
-                     <label htmlFor="subject">Dia da semana</label>
-                     <input type="text" id="week_day" />
-                   </div>
-
-                   <div className="input-block">
-                     <label htmlFor="subject">Hora</label>
-                     <input type="text" id="time" />
-                   </div>
-                </form>
+              <form id="search-teachers">
+                <Input name="subject" label="Matéria"/>
+                <Input name="week_day" label="Dia da semana"/>
+                <Input type="time" name="time" label="Hora"/>
+              </form>
             </PageHeader>
 
             <main>
